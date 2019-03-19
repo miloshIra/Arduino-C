119f5bf2e8d9439a1437b0dc6e73769232e7b221
Milosh Aritonski <30418161+miloshIra@users.noreply.github.com>
2019-03-19 21:34:53 +0100

    cash 6 funkcii

---
+++ Course/cls6-f.c
@@ -0,0 +1,17 @@
+    #include <stdio.h>
+    #include <stdlib.h>
+    #define MAX 100
+
+
+    void pozdravi_me () {         // void е функција без враќање на аргумент како int!! (нема return)!!
+        printf("Zdravo, Svetu!\n");
+
+    }
+
+
+
+    int main()
+    {
+       pozdravi_me();
+        return 0;
+        }
