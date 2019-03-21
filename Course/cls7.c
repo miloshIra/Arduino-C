98ead2f950cd98716df2ceab36293f0c3ead9792
Milosh Aritonski <30418161+miloshIra@users.noreply.github.com>
2019-03-21 19:40:56 +0100

    Create cls7.c

---
+++ Course/cls7.c
@@ -0,0 +1,15 @@
+    #include <stdio.h>
+    #include <stdlib.h>
+
+    int test(int arg) {
+
+        return 1;
+
+    }
+
+
+    int main()
+    {
+        int prom = int test(67);   // return 1 za da vrati nesto i da se zacuva vo int prom.
+        return 0;
+    }
